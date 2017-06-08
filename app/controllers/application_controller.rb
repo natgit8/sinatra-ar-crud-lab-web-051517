89838ead518ad46f7aa5f6cccf6305ca74bd4a9b
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-06-07 22:18:00 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,43 @@
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
-
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+  get '/posts/new' do
+    erb :new
+    #renders new form
   end
 
-  get '/' do 
-    
+  post '/posts' do
+    @post = Post.create(params)
+    redirect to '/posts'
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do #loads edit form
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do  #updates a post
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+
+  delete '/posts/:id/delete' do #delete action
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    redirect to '/posts'
   end
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,1 +1,13 @@
 #Placeholder for a model
+class Post < ActiveRecord::Base
+  # attr_accessor :name, :content
+  #
+  # POSTS = []
+  #
+  # def initialize(params)
+  #   @name = params[:name]
+  #   @content = params[:content]
+  #   POSTS << self
+  # end
+
+end
